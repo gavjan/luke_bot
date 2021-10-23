a1a0d647a4aedcac528cebbb9401dae7d9bf924b
gavjan <gavjan@github.com>
2021-10-23 15:20:09 +0400

    sedon

---
+++ run.sh
@@ -0,0 +1,23 @@
+send_mail() {
+  echo "Sending error mail..."
+  sed -i "s/\"/'/g" .err_file
+  curl "https://api.postmarkapp.com/email" \
+  -X POST \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "X-Postmark-Server-Token: $(cat .env/mail_token.env)" \
+  -d "{
+  \"From\": \"$(cat .env/mail.env)\",
+  \"To\": \"$(cat .env/mail.env)\",
+  \"Subject\": \"[Luke] Bot Error\",
+  \"TextBody\": \"$(cat .err_file)\",
+  \"MessageStream\": \"outbound\"
+  }"
+}
+
+
+while true; do
+  cd src || exit 1
+  python3 main.py 2>.err_file || send_mail
+  git pull
+done
