c86de7b21e76b7a077069b62188df94440be4d63
gavjan <gavjan@github.com>
2021-10-24 22:41:24 +0400

    daily_verse

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from cons import *
-from query import parse_query
+from query import parse_query, daily_verse
+from datetime import datetime, time, timedelta
+import asyncio
 import discord
 
 
 def main():
     client = discord.Client()
+    WHEN = time(4, 0, 0)  # 4:00 AM UTC
+    default_channel_id = 456178384016244738
 
     @client.event
     async def on_ready():
@@ -31,6 +35,28 @@ def main():
         except Exception as e:
             err_exit(e)
 
+    async def called_once_a_day():
+        await client.wait_until_ready()
+        channel = client.get_channel(default_channel_id)
+        await channel.send(embed=daily_verse())
+
+    async def background_task():
+        now = datetime.utcnow()
+        if now.time() > WHEN:
+            tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+            seconds = (tomorrow - now).total_seconds()
+            await asyncio.sleep(seconds)
+        while True:
+            now = datetime.utcnow()
+            target_time = datetime.combine(now.date(), WHEN)
+            seconds_until_target = (target_time - now).total_seconds()
+            await asyncio.sleep(seconds_until_target)
+            await called_once_a_day()
+            tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
+            seconds = (tomorrow - now).total_seconds()
+            await asyncio.sleep(seconds)
+
+    client.loop.create_task(background_task())
     client.run(getenv("bot_token"))
 
 
