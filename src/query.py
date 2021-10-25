56bd1906304f935b9f15b4811b05e256afd27d77
gavjan <gavjan@github.com>
2021-10-25 16:28:54 +0400

    daily_holiday

--- src/query.py
+++ src/query.py
@@ -164,7 +164,7 @@ def daily_verse():
 
 def parse_query(query, debug=False):
     content = query if debug else query.content
-    if re.match(r"^\s*/test_holiday\s*", content):
+    if re.match(r"^\s*/test_holiday\s*", content) and query.author.id == ADMIN_ID:
         return todays_holiday()
     if re.match(r"^\s*/verse\s*", content):
         return parse_verse(content)
