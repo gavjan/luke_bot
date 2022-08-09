fd0e05329862f04b2ae4c2ed0f78cb5b75e04b44
Gav <gevochobanyan@gmail.com>
2022-08-09 13:55:43 +0200

    ...

--- src/query.py
+++ src/query.py
@@ -248,6 +248,7 @@ def parse_query(query, debug=False):
     content = query if debug else query.content
     ret = []
     if query.channel.id == 1002183766682390539:
+        ret.append((actions.REACT, ["hrrrr"]))
         ret.append(assert_count(content))
     if re.match(r"^\s*/test_holiday\s*$", content) and query.author.id == ADMIN_ID:
         ret.append((todays_holiday()))
