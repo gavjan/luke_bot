bd12d73c52cd40a887dca4243115480b0b0cf36c
gavjan <gavjan@github.com>
2023-03-21 11:53:06 +0100

    strook final_test

--- src/query.py
+++ src/query.py
@@ -303,8 +303,7 @@ async def parse_query(query, client, debug=False):
     if query.author.id == TUS_ID:
         ret.append((actions.REACT, ["tus"]))
     if STRUK_ID in [x.id for x in query.author.roles]:
-        #ret.append((actions.REPLY, f"channel={query.channel_id}, message={query.message_id}"))
-        ret.append((actions.REPLY, f"{query}"))
+        ret.append((actions.REPLY, f"channel={query.channel.id}, message={query.id}"))
         # await rm_message(client, query.channel_id, query.message_id)
         ret.append((actions.REACT, ["🇳"]))
     elif re.search(r"(\W|_|\d|^)(gn|գն|bg|բգ)(\W|_|\d|$)", content, flags=re.UNICODE | re.IGNORECASE):
