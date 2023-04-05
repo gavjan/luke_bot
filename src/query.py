ceac141a0368bb0fcd412f431f0262a646fcad73
Gav <gevochobanyan@gmail.com>
2023-04-05 04:19:45 -0700

    rm_tus thread message

--- src/query.py
+++ src/query.py
@@ -320,6 +320,8 @@ async def parse_query(query, client, debug=False):
         ret.append((actions.REACT, ["tus"]))
     if STRUK_ID in [x.id for x in query.author.roles]:
         await rm_message(client, query.channel.id, query.id)
+    if query.channel.id == TUS_THREAD_ID:
+        await rm_message(client, query.channel.id, query.id)
     elif re.search(r"(\W|_|\d|^)(gn|գն|bg|բգ)(\W|_|\d|$)", content, flags=re.UNICODE | re.IGNORECASE):
         ret.append((actions.REACT, ["🇬", "🇳", "gandz"]))
     if re.match(r"^s*/restart_luke\s*$", content) and query.author.id in ADMIN_IDS:
