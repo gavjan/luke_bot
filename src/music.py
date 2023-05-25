0f99e323c0d69c80f5404747e6d02230596bc885
gavjan <gavjan@github.com>
2023-05-25 19:52:32 +0200

    Playing with query

--- src/music.py
+++ src/music.py
@@ -5,11 +5,44 @@ from discord.utils import get
 import yt_dlp as youtube_dl
 
 
+def parse_youtube_link(url):
+    youtube_re = r'((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?'
+    match = re.search(youtube_re, url)
+    
+    if not match: return None
+    
+    return {
+        "protocol": match.group(1),
+        "subdomain": match.group(2),
+        "domain": match.group(3),
+        "is_nocookie": bool(match.group(4)),
+        "path": match.group(5),
+        "video_id": match.group(6),
+        "query_string": match.group(7)
+    }
+    
+def get_youtube_url(search_query):
+    ydl_opts = {
+        'default_search': 'ytsearch1',
+        'format': 'bestaudio/best',
+        'quiet': True
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        search_results = ydl.extract_info(search_query, download=False)
+        if 'entries' in search_results:
+            video_url = search_results['entries'][0]['webpage_url']
+            return video_url
+        else:
+            return None
+
+
 async def leave(client, message, _):
      for x in client.voice_clients:
-        if(x.server_id == message.guild.id):
+        print(x)
+        if x.server_id == message.guild.id:
             await x.disconnect()
         return actions.REACT, "✅"
+     return actions.ERR, discord.Embed(description=f"I'm not in a VC",)
      
 
 async def join(client, message, voice):
@@ -24,15 +57,12 @@ async def join(client, message, voice):
             await leave(client, message, voice)
             return await voice.channel.connect()
             
-async def play_url(client, message, voice):
+async def play_url(client, message, voice, url):
     ret = await join(client, message, voice)
     if not isinstance(ret, discord.voice_client.VoiceClient):
         return ret
     voice_client = ret
-    url = message.content.replace("./play_url ", "")
 
-
-    # Download the audio using youtube_dl
     ydl_opts = {
         'format': 'bestaudio/best'
     }
@@ -47,34 +77,55 @@ async def play_url(client, message, voice):
     return actions.REACT, "✅"
 
 async def play(client, message, voice):
-    return actions.REPLY, "./play is under development. pls use ./play_url for now"
-    ret = await join(client, message, voice)
-    if not isinstance(ret, discord.voice_client.VoiceClient):
+    parsed = parse_youtube_link(message.content)
+    if parsed:
+        url = f'https://www.youtube.com/watch?v={parsed["video_id"]}"'
+        return await play_url(client, message, voice, url)
+    
+    # Leave only search query
+    query = message.content
+    match = re.match(r"^\s*\./play\s+", message.content)
+    query = query[match.end():]
+
+    url = get_youtube_url(query)
+    if not url:
+        return actions.ERR, discord.Embed(description=f"Couldn't find a youtube video matching your query",)
+
+    ret = await play_url(client, message, voice, url)
+    if ret[0] == actions.ERR:
         return ret
-    voice_client = ret
-    
-    audio_source = discord.FFmpegPCMAudio('/home/cgev/Desktop/song.mp3')
-    voice_client.play(audio_source)
-    
-    
-    return actions.REACT, "✅"
+    return actions.REPLY, f"Playing {query}"
+
+
+async def get_queue(client, message, voice):
+    return actions.REPLY, "TODO"
 
 
 async def handle_music(client, message):
     voice = message.author.voice
-    if voice is None or not isinstance(voice, discord.member.VoiceState):
-         return actions.REPLY, "Join a voice channel first"
-    
     commands = [
-         (r"^\s*\./leave\s*$", leave),
-         (r"^\s*\./join\s*$", join),
-         (r"^\s*\./play\s+", play),
-         (r"^\s*\./play_url\s+", play_url),
+        (r"^\s*\./leave\s*$", leave),
+        (r"^\s*\./queue\s*$", get_queue),
     ]
-
     for rgx, func in commands:
         if re.match(rgx, message.content):
             return await func(client, message, voice)
+
+    
+    vc_commands = [
+         (r"^\s*\./join\s*$", join),
+         (r"^\s*\./play\s+", play),
+    ]
+    not_in_voice = voice is None or not isinstance(voice, discord.member.VoiceState)
+    for rgx, func in vc_commands:
+
+        if re.match(rgx, message.content):
+            if not_in_voice:
+                return actions.REPLY, "Join a voice channel first"
+            else:
+                return await func(client, message, voice)
+        
+    
     
     err_embed = discord.Embed(title="*wrong music syntax*", description="see: `?help`",)
 
