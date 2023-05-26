af432b613e6ce4f5bfc1fe23f95d4e0a6219f689
gavjan <gavjan@github.com>
2023-05-26 19:53:08 +0200

    queue prototype

--- src/music.py
+++ src/music.py
@@ -2,7 +2,17 @@ import discord
 import re
 from cons import *
 from discord.utils import get
+
 import yt_dlp as youtube_dl
+import asyncio
+import time
+
+
+
+
+
+queue = asyncio.Queue()
+voice_client = None
 
 
 def parse_youtube_link(url):
@@ -43,41 +53,63 @@ def correct_levitating(query, url):
 
 async def leave(client, message, _):
      for x in client.voice_clients:
-        print(x)
         if x.server_id == message.guild.id:
             await x.disconnect()
         return actions.REACT, "✅"
      return actions.ERR, discord.Embed(description=f"I'm not in a VC",)
      
 
-async def join(client, message, voice):
+async def join(client, message, voice, url_to_play=None):
+        global voice_client
         perms = voice.channel.permissions_for(message.guild.me)
         if not perms.connect:
             err_embed = discord.Embed(title="*No Permission*", description=f"I can't connect to `{voice.channel}`",)
             return actions.ERR, err_embed
         try:
-            return await voice.channel.connect()
+            voice_client = await voice.channel.connect()
             
         except discord.ClientException:
             await leave(client, message, voice)
-            return await voice.channel.connect()
+            voice_client = await voice.channel.connect()
+        
+        if url_to_play is not None:
+            await queue.put(url_to_play)
+        
+        while True:
+            url = await queue.get()
+            await play_url(voice_client, url)
             
-async def play_url(client, message, voice, url):
-    ret = await join(client, message, voice)
-    if not isinstance(ret, discord.voice_client.VoiceClient):
-        return ret
-    voice_client = ret
+            while voice_client.is_playing(): await asyncio.sleep(1) # Father forgive me for I have sinned
+
+        await leave()
+        return actions.SEND, "Idle timeout; Leaving vc."
+
+async def add_url(client,message, voice, url):
+    global voice_client
+    if voice_client is None:
+        return await join(client, message, voice, url)
+    await queue.put(url)
+    return actions.SEND, "Adding to queue for master"
+
+async def play_url(vc, url):
+    def after_playing(error):
+        if error:
+            print('Error while playing song:', error)
+        else:
+            print('Song finished playing')
+        
 
     ydl_opts = {
-        'format': 'bestaudio/best'
+        'format': 'best[height<=480]',
+        'skip_download': True,
+        'youtube_include_dash_manifest': False,
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
         audio_url = info_dict.get("url", None)
 
     audio_source = discord.FFmpegPCMAudio(audio_url)
-    voice_client.play(audio_source)
-    
+    vc.play(audio_source, after=after_playing)
     
     return actions.REACT, "✅"
 
@@ -85,7 +117,7 @@ async def play(client, message, voice):
     parsed = parse_youtube_link(message.content)
     if parsed:
         url = f'https://www.youtube.com/watch?v={parsed["video_id"]}"'
-        return await play_url(client, message, voice, url)
+        return await add_url(client, message, voice, url)
     
     # Leave only search query
     query = message.content
@@ -98,19 +130,32 @@ async def play(client, message, voice):
     
     url = correct_levitating(query, url)
 
-    ret = await play_url(client, message, voice, url)
+    ret = await add_url(client, message, voice, url)
     if ret[0] == actions.ERR:
         return ret
     return actions.REPLY, f"Playing {query}"
 
 
+async def test_push(client, message, voice):
+    queue.put(message.content)
+    return actions.REPLY, f"{queue.qsize()}"
+
+async def test_pop(client, message, voice):
+    return actions.REPLY, f"{queue.get()}"
+
+
 async def get_queue(client, message, voice):
-    return actions.REPLY, "TODO"
+    global queue
+    tmp = queue
+    queue = message.content
+    return actions.REPLY, tmp
 
 
 async def handle_music(client, message):
     voice = message.author.voice
     commands = [
+        (r"^\s*\./push\s+", test_push),
+        (r"^\s*\./pop\s*", test_pop),
         (r"^\s*\./leave\s*$", leave),
         (r"^\s*\./queue\s*$", get_queue),
     ]
@@ -118,9 +163,11 @@ async def handle_music(client, message):
         if re.match(rgx, message.content):
             return await func(client, message, voice)
 
-    
-    vc_commands = [
-         (r"^\s*\./join\s*$", join),
+    if re.match(r"^\s*\./join\s*$", message.content):
+        asyncio.create_task(join(client, message, voice))
+        return actions.IGNORE, None
+
+    vc_commands = [ 
          (r"^\s*\./play\s+", play),
     ]
     not_in_voice = voice is None or not isinstance(voice, discord.member.VoiceState)
