80cd193167f6b35c48b003672c32c4fed90202f8
gavjan <gavjan@github.com>
2025-04-04 12:53:59 +0200

    important emojis

--- src/music.py
+++ src/music.py
@@ -285,7 +285,7 @@ async def join(client, message, voice, urls_to_play=None):
                 if not url: url = "Text to Speech"
                 now_playing = await message.channel.send(f" Now playing {url}")
             else:
-                err_text = f"Caused by: {url}\nPossible problems:\n- Age-Restricted video\n- Unsupported format\n- YouTube hates me 🥺"
+                err_text = f"Caused by: {url}\nPossible problems:\n- 🔞 Age-Restricted video\n- ⛔ Unsupported format\n- 🥺 YouTube hates me"
                 err = discord.Embed(title="Player Crashed. Restarting.", description=err_text)
                 err.color = discord.Color.red()
                 await message.channel.send(embed=err)
