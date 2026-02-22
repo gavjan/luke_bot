76a897e14641f08a8533385c5a2bbc7ba78b3d9f
gavjan <gavjan@github.com>
2026-02-22 17:44:07 +0100

    Revert "Merge pull request #6 from gavjan/FIX/add-react-delay"
    This reverts commit 6f23b759f0fe804b83b702f44669b3e6249a578e, reversing
    changes made to 33faea761a473e84a245baf86c0ac5e5ca6d1ab0.

--- src/main.py
+++ src/main.py
@@ -63,7 +63,6 @@ def main():
                 elif action == actions.REACT:
                     for emoji_id in response:
                         emoji = get(client.emojis, name=emoji_id)
-                        await asyncio.sleep(0.3) # discord bugs out and doesn't display these reacts client-side
                         await message.add_reaction(emoji or emoji_id)
                 elif action == actions.BUTTONS:
                     sent = await message.reply(embed=response["embed"])
